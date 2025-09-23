07755c27bfabfccf109f8a3ff090feb07e91e745
arthur-nepomuceno <arth.nepomuceno@gmail.com>
2025-09-23 20:41:55 -0300

    feat: Remover todos os arquivos do repositório

--- questora-nextjs/src/data/questions.ts
+++
@@ -1,8 +0,0 @@
-import { Question } from '@/types/quiz';
-import futebolQuestions from './futebol_100.json';
-import novelasQuestions from './novelas-100.json';
-
-export const questionsData: { [key: string]: Question[] } = {
-  futebol: futebolQuestions as Question[],
-  novelas: novelasQuestions as Question[]
-};
